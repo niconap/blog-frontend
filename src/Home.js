e9f5a087f229151cf5c236ec23e0b5ecac58e2a7
Nico Nap <nico.nap@outlook.com>
2021-08-15 09:03:41 +0200

    Create home page

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Articles from './Articles';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my blog!</h1>
+      <Articles />
+    </div>
+  );
+}
+
+export default Home;
